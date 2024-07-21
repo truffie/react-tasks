d6d165145d5ab71a841883642da7aa33c020bc2e
Truffie <trufanov.vl2013@gmail.com>
2024-07-21 03:16:20 +0300

    fix: add validation

--- src/pages/useState/Task6/Task6.jsx
+++ src/pages/useState/Task6/Task6.jsx
@@ -1,96 +1,127 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export function Task6() {
   const [result, setResult] = useState({
-    prev: "",
-    current: "",
+    prev: '',
+    current: '',
     action: null,
-    result: "",
+    result: '',
   });
 
   const actions = {
-    "+": (a, b) => a + b,
-    "-": (a, b) => a - b,
-    "*": (a, b) => a * b,
-    "/": (a, b) => a / b,
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => (isNaN(a / b) ? 'infinity value' : a / b),
   };
 
   function changeCurrentValue(e) {
-    let value = e.target.value;
-
-    if (isNaN(+value)) {
+    let value = Number(e.target.value);
+    if (isNaN(value)) {
       return;
     }
 
     setResult((prev) => ({
       ...prev,
-      result: prev.result && prev.prev ? prev.result : "",
-      current: +value,
+      current: value,
     }));
   }
 
   function handleCalcAction(action) {
-    if (!result.current && typeof result.current === "string") {
+    if (!isFinite(result.current)) {
       return;
     }
-    if (!result.prev && typeof result.current !== "string") {
+    if (!result.prev) {
       setResult((prev) => ({
         prev: prev.current,
-        current: "",
+        current: '',
         action,
       }));
     }
 
-    if (result.prev && typeof result.current !== "string") {
+    if (result.prev) {
       setResult((prev) => ({
         action,
-        result: prev.result
-          ? actions[prev.action](prev.result, prev.current)
-          : actions[prev.action](prev.prev, prev.current),
         prev: prev.result ? prev.result : prev.current,
-        current: "",
+        result: actions[prev.action](prev.result || prev.prev, prev.current),
+        current: '',
       }));
     }
   }
 
   function resetAll() {
     setResult({
-      prev: "",
-      current: "",
+      prev: '',
+      current: '',
       action: null,
-      result: "",
+      result: '',
     });
   }
 
   function showResult() {
-    if (!isNaN(result.prev) && !isNaN(result.current)) {
+    if (isFinite(result.prev) && isFinite(result.current)) {
       setResult((prev) => ({
         ...prev,
-        prev: "",
-        current: "",
-        result: prev.result
-          ? actions[prev.action](prev.result, prev.current)
-          : actions[prev.action](prev.prev, prev.current),
+        prev: '',
+        current: '',
+        log: console.log(prev),
+        result: actions[prev.action](prev.result || prev.prev, prev.current),
       }));
     }
   }
 
   return (
     <>
-      <h2>Создайте компонент простого калькулятора, который выполняет команды: +, -, *, /</h2>
+      <h2>
+        Создайте компонент простого калькулятора, который выполняет команды: +,
+        -, *, /
+      </h2>
 
-      <div>
-        <h3>result: {result.result}</h3>
-        <h4>prev value: {result.prev}</h4>
-        <input type="text" placeholder="enter value" onChange={changeCurrentValue} value={result.current} />
-        <div style={{ display: "flex", gap: "10px" }}>
+      <div
+        style={{
+          display: 'flex',
+          gap: '10px',
+          margin: '25px',
+          padding: '25px 10px',
+          border: '2px solid black',
+          borderRadius: '15px',
+        }}
+      >
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px',
+            fontSize: '1.5rem',
+          }}
+        >
+          <input
+            type="text"
+            placeholder="enter value"
+            onChange={changeCurrentValue}
+            value={result.current}
+            style={{ fontSize: 'inherit', height: '50px' }}
+          />
+          <h3>result: {result.result}</h3>
+          <h4>previous value: {result.prev}</h4>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
           {Object.keys(actions).map((action, i) => (
-            <button key={i} onClick={() => handleCalcAction(action)}>
+            <button
+              className="actionLink"
+              key={i}
+              onClick={() => handleCalcAction(action)}
+            >
               {action}
             </button>
           ))}
-          <button onClick={showResult}>=</button>
-          <button onClick={resetAll}>C</button>
+          <button className="actionLink" onClick={showResult}>
+            =
+          </button>
+          <button className="actionLink" onClick={resetAll}>
+            C
+          </button>
         </div>
       </div>
     </>
